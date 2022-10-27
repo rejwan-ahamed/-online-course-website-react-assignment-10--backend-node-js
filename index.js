efea3d0214f2cbb77fc252e8f6f841e641a94b51
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-10-27 19:01:02 +0600

    setup server code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const port = 5002;
+
+app.use(cors());
+
+const courseDB = require("./course.json");
+
+app.get("/course", (req, res) => {
+  res.send(courseDB);
+});
+
+app.get("/course/:id", (req, res) => {
+  const ID = req.params.id;
+  const courseData = courseDB.find((course) => course.id == ID);
+  res.send(courseData);
+});
+
+app.listen(port, () => {
+  console.log(`Api listening port ${port}`);
+});
